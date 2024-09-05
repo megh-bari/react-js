b3508b2ab41903263a9863ef1d015741906fc005
megh-bari <meghbari01@gmail.com>
2024-09-05 23:15:46 +0530

    Virtual DOM | Fibre

---
+++ virtual-dom/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <p>Click on below link to read React Fibre</p>
+      <a href='https://github.com/acdlite/react-fiber-architecture' target='_blank'>React Fibre</a>
+    </>
+  )
+}
+
+export default App
