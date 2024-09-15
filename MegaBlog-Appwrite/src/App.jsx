aa5022dfdf9a7485dd971e83b6efae43ba408c5f
megh-bari <meghbari01@gmail.com>
2024-09-15 21:00:27 +0530

    env and appwrite setup

---
+++ MegaBlog-Appwrite/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_ENDPOINT_URL);
+  return (
+    <>
+      <h1 className="text-center">A blog with Appwrite</h1>
+    </>
+  );
+}
+
+export default App;
